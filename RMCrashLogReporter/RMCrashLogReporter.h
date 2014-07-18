fdf0bb4d0ce98ed5dfe4c84ab7628f236b4be93c
高佶 <ji.gao@renren-inc.com>
2014-07-18 15:50:47 +0800

    更改名字

---
+++ RMCrashLogReporter/RMCrashLogReporter.h
@@ -0,0 +1,13 @@
+//
+//  RMCrashLogReporter.h
+//  RMCrashLogReporter
+//
+//  Created by leave on 14-7-18.
+//  Copyright (c) 2014年 leave. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RMCrashLogReporter : NSObject
+
+@end
---
+++ RMCrashLogReporter/RMCrashLogReporter.m
@@ -0,0 +1,13 @@
+//
+//  RMCrashLogReporter.m
+//  RMCrashLogReporter
+//
+//  Created by leave on 14-7-18.
+//  Copyright (c) 2014年 leave. All rights reserved.
+//
+
+#import "RMCrashLogReporter.h"
+
+@implementation RMCrashLogReporter
+
+@end
